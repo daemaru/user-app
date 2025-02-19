7e54eb4ae6b81671d31013402e5e470efa58016b
wlals <jiminelp@gmail.com>
2025-02-19 10:58:27 +0900

    feat :: enum 타입 정의

---
+++ src/renderer/src/types/enum.ts
@@ -0,0 +1,16 @@
+export const DayOfTheWeek = ['월', '화', '수', '목', '금', '토', '일']
+export const Example = ['다문화 이해 교육', '모의 토익', '기말고사']
+export const Months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
